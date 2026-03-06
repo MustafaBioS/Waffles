4ab3498664ed6c41558528c3cd9bbda794c44f72
MustafaBioS <mustaafahany@gmail.com>
2026-03-06 07:58:26 +0200

    ADD - Navbar Animation and Functionality

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const viewCount = document.querySelector('.viewCount');
 const count = document.querySelector('.count');
+const home = document.querySelector('.home');
+const projects = document.querySelector('.projects');
 
 fetch("https://countapi.mileshilliard.com/api/v1/hit/Views")
     .then(res => res.json())
     .then(data => {
         count.textContent = `Count: ${data.value}`;
     })
+
+if (location.pathname === "/index.html") {
+    home.classList.add('chosen');
+} else if (location.pathname === "/projects.html") {
+    projects.classList.add('chosen');
+}
